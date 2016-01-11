5fbd3a91511f29f97eba82532bb33a3212fac0c1
KSXGitHub <hvksmr1996@gmail.com>
2016-01-11 22:38:37 +0700

    add try-catch

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 	module.exports = {
 		chainFunction: require('./chain-function.js'),
-		ternary: require('./ternary.js')
+		ternary: require('./ternary.js'),
+		tryCatch: require('./try-catch.js')
 	};
 
 })(module);

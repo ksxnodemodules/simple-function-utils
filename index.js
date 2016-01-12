caaaaefb0aec611912ce7ac768b8838834edb977
KSXGitHub <hvksmr1996@gmail.com>
2016-01-12 09:50:50 +0700

    add functionize-class

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 	module.exports = {
 		bind: require('./bind.js'),
 		chainFunction: require('./chain-function.js'),
+		functionizeClass: require('./functionize-class.js'),
 		transform: require('./transform.js'),
 		ternary: require('./ternary.js'),
 		tryCatch: require('./try-catch.js')

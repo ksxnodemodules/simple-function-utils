ce4fe3b406d9a0fc18ba9310450debe72efc0008
KSXGitHub <hvksmr1996@gmail.com>
2016-01-12 10:53:38 +0700

    improve bind

--- bind.js
+++ bind.js
@@ -2,14 +2,17 @@
 ((module) => {
 	'use strict';
 
-	var bind = module.exports = (type, ...args) =>
-		bind[type ? 'begin' : 'end'](...args);
+	var bind = module.exports = (fn, begin, end) =>
+		bind.begin(bind.end(fn, ...end), ...begin);
 
 	bind.begin = (fn, ...args) =>
 		(...rest) => fn(...args, ...rest);
 
 	bind.end = (fn, ...args) =>
 		(...rest) => fn(...rest, ...args);
+
+	bind.middle = (fn, begin, end, ...middle) =>
+		(...args) => fn(...[...args.slice(0, begin), ...middle, ...args.slice(-end)]);
 
 	bind.pack = (fn) =>
 		(...args) => fn(args);

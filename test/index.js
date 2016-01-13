1bda7b411350d90d04930a1802666266587304e8
KSXGitHub <hvksmr1996@gmail.com>
2016-01-13 10:11:16 +0700

    add test dir

---
+++ test/index.js
@@ -0,0 +1,10 @@
+
+((module) => {
+	'use strict';
+
+	// var sfu = require('simple-function-utils');
+	// var out = module.exports = {};
+
+	module.exports = 'Everything is passed, test later!';
+
+})(module);

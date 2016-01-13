0071db3beab77a62d25ca40dca553605f434f73a
KSXGitHub <hvksmr1996@gmail.com>
2016-01-13 09:49:07 +0700

    complete culmulate

--- culmulate.js
+++ culmulate.js
@@ -2,6 +2,14 @@
 ((module) => {
 	'use strict';
 
-	
+	var culmulate = module.exports = (fn, left, right, ...rest) =>
+		rest.length ? culmulate(fn, right, ...rest) : fn(left, right);
+
+	culmulate.of = (fn) =>
+		(...operand) => culmulate(fn, ...operand);
+
+	culmulate.summary = culmulate.of((a, b) => a + b);
+
+	culmulate.product = culmulate.of((a, b) => a * b);
 
 })(module);

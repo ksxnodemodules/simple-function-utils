3207e7edd13fc8c4aab0b34b548f4978df65f97f
KSXGitHub <hvksmr1996@gmail.com>
2016-01-22 20:30:56 +0700

    update

--- test/index.js
+++ test/index.js
@@ -2,9 +2,32 @@
 ((module) => {
 	'use strict';
 
-	// var sfu = require('simple-function-utils');
-	// var out = module.exports = {};
+	var sfu = require('simple-function-utils');
+	var out = module.exports = {};
 
-	module.exports = 'Everything is passed, test later!';
+	out.message = 'This test is incomplete';
+
+	[
+		() => {
+			var mname = 'sfu.boolean';
+			var mkArg = (object, pname) =>
+				() => object[pname];
+			var testBinary = (pname) => {
+				
+			};
+			var result = {};
+			['and', 'or', 'xnor', 'xor'].forEach(testBinary);
+			['and', 'or'].forEach(testSerial);
+			return new TestResult(mname, result);
+		}
+	].forEach((test) => {
+		var result = test();
+		out[result.name] = result.value;
+	});
+
+	function TestResult(name, value) {
+		this.name = name;
+		this.value = value;
+	}
 
 })(module);

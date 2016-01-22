b984581c250e33fb531c6a38315c9925d3ed129a
KSXGitHub <hvksmr1996@gmail.com>
2016-01-22 21:02:11 +0700

    update

--- test/index.js
+++ test/index.js
@@ -10,11 +10,21 @@
 	[
 		() => {
 			var mname = 'sfu.boolean';
-			var mkArg = (object, pname) =>
-				() => object[pname];
+			var ProductIterable = require('x-iterable/product-iterable');
 			var testBinary = (pname) => {
-				
+				var sub = result[`${mname}.${pname}`] = {};
+				var mkfn = sfu.boolean[pname];
+				ProductIterable.pow([false, true], 2).forEach(subtest(sub, mkfn));
 			};
+			var testSerial = (pname) => {
+				var sub = result[`${mname}.${pname}.serial`] = {};
+				var mkfn = sfu.boolean[pname].serial;
+				ProductIterable.pow([0, 1], 4).forEach(subtest(sub, mkfn));
+			};
+			var mkargs = (array) =>
+				array.map((x) => () => x);
+			var subtest = (sub, mkfn) =>
+				(array) => sub[JSON.stringify(array)] = mkfn(...mkargs(array))();
 			var result = {};
 			['and', 'or', 'xnor', 'xor'].forEach(testBinary);
 			['and', 'or'].forEach(testSerial);
